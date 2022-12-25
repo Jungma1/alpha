64b59e392f5b8be0c2e8964dca95b33487adc7a1
Jungma <mbjun3630@naver.com>
2022-12-25 17:58:56 +0900

    1 - 약수의 개수와 덧셈

---
+++ basic/sumOfDivisors.js
@@ -0,0 +1,16 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/77884
+
+function solution(left, right) {
+  let answer = 0;
+
+  for (let a = left; a <= right; a++) {
+    const count = new Array(a)
+      .fill(0)
+      .map((n, i) => i + 1)
+      .filter(n => a % n === 0).length;
+
+    count % 2 === 0 ? (answer += a) : (answer -= a);
+  }
+
+  return answer;
+}
